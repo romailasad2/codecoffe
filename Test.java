ad730e21373feb61e5e45fc2bb7ddfa59828f77d
usamakhizar <57050437+usamakhizar@users.noreply.github.com>
2019-10-27 14:10:03 +0500

    v-1.2

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Hello World!");
         System.out.println("Sb ka Maalik Aeek!!!");
         System.out.println("Change");
+        System.out.println("Pak wheels");
     }
 }
