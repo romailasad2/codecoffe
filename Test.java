2870708ebb8320f9812881d5bfaa4e78ff9b4dd9
romailasad2 <53368470+romailasad2@users.noreply.github.com>
2019-10-27 14:05:37 +0500

    Update Test.java
    added one sout

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String [] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Sb ka Maalik Aeek!!!");
         System.out.println("Change");
     }
 }
